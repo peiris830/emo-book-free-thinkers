f7f1a81fb0686701498d5ffd70c28cd8a7d0761a
peiris830 <tharushapeiris120@gmail.com>
2023-04-10 03:25:04 +0530

    created a connection to mongoDB database

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+require('dotenv').config({path: '../.env'});
+
+connectDB();
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');  
 
-const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const path = require("path");
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+            .then(() => console.log('MongoDB Connected'));
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
