1955594257af058975841cce220fec5c8b7d5509
peiris830 <tharushapeiris120@gmail.com>
2023-04-10 09:09:41 +0530

    resolved the error of connecting to the database

--- backend/app.js
+++ backend/app.js
@@ -2,22 +2,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/user');
 require('dotenv').config({path: '../.env'});
 
 connectDB();
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');  
-
-const userRoutes = require('./routes/user');
+/*app.set('view engine', 'ejs');
+app.set('views', 'views');*/
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use(adminRoutes);
-app.use(userRoutes);*/
+app.use(userRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,20 @@
+const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const path = require("path");
 require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        /*const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
         })
             .then(() => console.log('MongoDB Connected'));
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);*/
+        const client = await mongodb.MongoClient.connect(process.env.MONGO_URI)
+            .then(() => console.log('MongoDB Connected'));
     } catch (error) {
         console.error(error);
         process.exit(1);
